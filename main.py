1a9b8957cf879a0dd564f287875686b70adcb86b
piotrek <piotrek@ubuntu.com>
2025-03-02 15:37:28 +0100

    works but not in all cases

--- main.py
+++ main.py
@@ -10,9 +10,14 @@ OBSTACLE_COLOR = ''
 GRAY = (82, 82, 82)
 WHITE = (255, 255, 255)
 
+def start():
+    driver.get(URL)
+    time.sleep(1)
+    pyautogui.press('space')
+    time.sleep(3)
 
 def is_white_bg() -> bool:
-    im_bg = pyautogui.screenshot(region=(110, 940, 10, 10))
+    im_bg = pyautogui.screenshot(region=(200, 950, 5, 5))
     im_bg.save('bg.jpg')
     img_bg = Image.open('bg.jpg')
     width_bg = img_bg.width
@@ -58,43 +63,45 @@ def is_bird():
             if img.getpixel(xy=(x, y)) == OBSTACLE_COLOR:
                 return True
 
+
 def is_game_over():
-    im=pyautogui.screenshot(region=(590,620,260,35))
-    im.save('game_ver.jpg')
-    img= Image.open('game_over.jpg')
+    im = pyautogui.screenshot(region=(590, 620, 260, 35))
+    im.save('game_over.jpg')
+    img = Image.open('game_over.jpg')
     for x in range(img.width):
         for y in range(img.height):
             if img.getpixel(xy=(x, y)) == OBSTACLE_COLOR:
                 return True
+
+
+def is_obstacle():
+    # im_cac = pyautogui.screenshot(region=(270, 740, 250, 100))
+    im_cac = pyautogui.screenshot(region=(272, 740, 232, 100))
+    im_cac.save('obs.jpg')
+    img = Image.open('obs.jpg')
+    width = img.size[0]
+    height = img.size[1]
+    for x in range(width):
+        for y in range(height):
+            if img.getpixel(xy=(x, y)) == OBSTACLE_COLOR:
+                return True
+
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option('detach', True)
 chrome_options.add_argument('--start-maximized')
 driver = webdriver.Chrome(options=chrome_options)
 
-driver.get(URL)
-time.sleep(1)
-pyautogui.press('space')
-time.sleep(2)
-
-
+start()
 game_is_on = True
 while game_is_on:
     if is_white_bg():
         OBSTACLE_COLOR = GRAY
     else:
-        OBSTACLE_COLOR = WHITE
+        OBSTACLE_COLOR = (171, 171, 171)
 
-    if is_cactus_far():
+    if is_obstacle():
         pyautogui.press('space')
-
-    if is_cactus_close():
-
-        pyautogui.press('space')
-
-
-    if is_bird():
-        print('jest ptak')
 
 # mechanizm gry
 # zrobic rzut ekranu w konkretnej pozycji i sprawdzic czy zawiera tam czarne pole
